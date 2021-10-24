e3833bdb8ecc78710a4de751d02e625628e96175
PorumbIonut <81915077+PorumbIonut@users.noreply.github.com>
2021-10-24 21:22:08 +0300

    .

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from UI.console import runMenu
 def main():
     runAllTests()
     lista = []
-    lista = adaugaRezervare("1", "Porumb Ionut", "business", "120", "nu", lista)
-    lista = adaugaRezervare("2", "Bogdan", "economy", "100", "da", lista)
     runMenu(lista)
 
 main()
