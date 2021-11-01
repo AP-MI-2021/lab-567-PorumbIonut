0abeed65424bcaf399909337d4f84ff0ba1d2833
PorumbIonut <81915077+PorumbIonut@users.noreply.github.com>
2021-11-01 19:36:46 +0200

    Am implementat o noua consola

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Logic.CRUD import adaugaRezervare
 from Tests.testAll import runAllTests
+from UI.command_line_console import runMenu2
 from UI.console import runMenu
 
 
 def main():
     runAllTests()
     lista = []
-    runMenu(lista)
-
+    #runMenu(lista)
+    runMenu2(lista)
 main()
