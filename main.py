f629b904d977519497b566f8cdf742bf1cff0323
PorumbIonut <81915077+PorumbIonut@users.noreply.github.com>
2021-11-01 20:55:55 +0200

    Am facut accesibile ambele main-uri

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from Tests.testAll import runAllTests
 from UI.command_line_console import runMenu2
 from UI.console import runMenu
 
+def optiuni():
+    print("1.Alege meniul standard")
+    print("2.Alege meniul in care poti apela mai multe functii in acelasi timp!")
 
 def main():
     runAllTests()
     lista = []
-    #runMenu(lista)
-    runMenu2(lista)
+    optiuni()
+    optiune = input("Alege main-ul pe care vrei sa-l rulezi: ")
+    if optiune == "1":
+        runMenu(lista)
+    elif optiune == "2":
+        runMenu2(lista)
+    else:
+        print("Meniul selectat nu exista! Reincercati! ")
+
 main()
